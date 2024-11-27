8820f336d8da989e91c52d95f9382c3206ebac75
Caroline Picanço Prockmann <117311079+carolprock@users.noreply.github.com>
2024-11-27 17:16:19 -0300

    Update config.ts

--- front-end/config.ts
+++ front-end/config.ts
@@ -1,5 +1,5 @@
-export const GOOGLE_MAPS_API_KEY = "AIzaSyAU5TWdgbuLamkwAQFvrCdZ65rHsskkp1M";
+export const GOOGLE_MAPS_API_KEY = "GOOGLE_MAPS_API_KEY";
 
-export const URL_MSPARKING = "https://c44a-2804-7f4-322a-f948-d419-a261-a75d-a903.ngrok-free.app";
+export const URL_MSPARKING = "ngrok_url";
 
-export const URL_APIGATEWAY = "http://192.168.0.11:3000";
+export const URL_APIGATEWAY = "EXPO_IP:3000"; // Atualizar com o IP usado no EXPO (Ex: 172.20.169.214)
